eb75e425f64c829f1d0b7e57b0031bba1504343f
gteramoto <102268319+gteramoto@users.noreply.github.com>
2022-11-23 21:15:14 -0300

    Create user_data.sh

---
+++ terraform/modules/back/scripts/user_data.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+yum update -y
+amazon-linux-extras install -y epel
+yum install -y amazon-efs-utils httpd telnet tree
+systemctl enable httpd
+service httpd start
+mkdir /mnt/efs
+echo "${efs_id}:/ /mnt/efs efs _netdev,noresvport,tls 0 0" >> /etc/fstab
+x=10
+while (( $x > 0 )); do
+  mount -fav
+  mnt=`df -h |grep /mnt/efs |wc -l`
+  if (( $mnt >= 1 )); then
+    [ ! -d "/mnt/efs/html" ] && mkdir /mnt/efs/html
+    echo "Notifier app backend" > /mnt/efs/html/index.html
+    rm -rf /var/www/html/
+    ln -s /mnt/efs/html/ /var/www/html
+    service httpd restart
+    break
+  fi
+  echo $((x--))
+  echo "Unable to mount EFS. Attempt: $x"
+  sleep 5
+done
