b2e9c71fe740b4968da1b4ba8c0a2e93d6555a3f
gteramoto <102268319+gteramoto@users.noreply.github.com>
2022-11-23 21:52:03 -0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,7 +11,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "tf-state-notifier-efs-bucket"
+    bucket         = "tf-state-notifier-efs-bucket02"
     key            = "terraform.tfstate"
     dynamodb_table = "tf-state-notifier-efs-table"
   }
